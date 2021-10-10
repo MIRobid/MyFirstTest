c8b765cb1efa69ac2e85b68ba297687a6ebc3ee2
mirobit.nasirov <mirobid_i@mail.ru>
2021-10-10 21:18:06 +0500

    Добавлены false методы и доп.тесты

--- app/src/test/java/com/example/android/myfirsttest/EmailValidatorTest.kt
+++ app/src/test/java/com/example/android/myfirsttest/EmailValidatorTest.kt
@@ -39,4 +39,46 @@ class EmailValidatorTest {
     fun emailValidator_NullEmail_ReturnsFalse() {
         Assert.assertFalse(EmailValidator.isValidEmail(null))
     }
+
+    @Test
+    fun emailValidator_InvalidEmailDoubleEmailSign_ReturnsFalse() {
+        Assert.assertFalse(EmailValidator.isValidEmail("@@email.com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNotLatinChars_ReturnsFalse() {
+        Assert.assertFalse(EmailValidator.isValidEmail("тест@мейл.ру"))
+    }
+
+    @Test
+    fun emailValidator_AssertEquals() {
+        Assert.assertEquals("test@mail.ru","test@mail.ru");
+    }
+
+    @Test
+    fun emailValidator_AssertNotEquals() {
+        Assert.assertNotEquals("test@mail.ru","test@mail.ru");
+    }
+
+    @Test
+    fun emailValidator_AssertArrayEquals() {
+        val x : IntArray= intArrayOf(1,2,3)
+        val y : IntArray= intArrayOf(1,2,3)
+        Assert.assertArrayEquals(x,y);
+    }
+
+    @Test
+    fun emailValidator_AssertNull() {
+        Assert.assertNull(EmailValidator);
+    }
+
+    @Test
+    fun emailValidator_AssertNotNull() {
+        Assert.assertNotNull(EmailValidator);
+    }
+
+    @Test
+    fun emailValidator_AssertSame() {
+        Assert.assertSame(EmailValidator,EmailValidator);
+    }
 }
